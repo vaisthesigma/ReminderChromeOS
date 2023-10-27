adbed4a01a0c31f4397c2b8abdaa3f7751b0f228
Arun Rao Balappa <arunraobalappa@gmail.com>
2023-10-27 16:38:07 +0530

    complete code overhaul again (x1)

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     const taskInput = document.getElementById("task");
-    const timeInput = document.getElementById("time");
-    const setReminderButton = document.getElementById("set-reminder");
-    const reminderList = document.getElementById("reminder-list");
+    const addTaskButton = document.getElementById("add-task");
+    const taskList = document.getElementById("task-list");
+    const completedTaskList = document.getElementById("completed-task-list");
 
-    setReminderButton.addEventListener("click", function () {
+    addTaskButton.addEventListener("click", function () {
         const task = taskInput.value;
-        const time = timeInput.value;
 
-        if (task && time) {
-            const reminderTime = new Date(time).toLocaleString();
-            const currentTime = new Date();
-            
-            if (currentTime < new Date(time)) {
-                const reminderItem = document.createElement("li");
-                reminderItem.textContent = `Task: ${task}, Time: ${reminderTime}`;
-                reminderList.appendChild(reminderItem);
+        if (task) {
+            const taskItem = document.createElement("li");
+            taskItem.textContent = task;
+            taskList.appendChild(taskItem);
 
-                // Play a beep sound
-                const beep = new Audio("beep.wav");
-                beep.play();
-
-                // Show a push notification
-                if (Notification.permission === "granted") {
-                    const notification = new Notification("Reminder", {
-                        body: `Task: ${task}`,
-                    });
+            taskItem.addEventListener("click", function () {
+                if (!taskItem.classList.contains("completed")) {
+                    taskItem.classList.add("completed");
+                    completedTaskList.appendChild(taskItem);
+                } else {
+                    taskItem.classList.remove("completed");
+                    taskList.appendChild(taskItem);
                 }
-            } else {
-                alert("Please choose a future time for your reminder.");
-            }
+            });
 
             taskInput.value = "";
-            timeInput.value = "";
-        } else {
-            alert("Please fill in both task and time fields.");
         }
     });
 });
