7acd367148e4051bfe41ace97e71ac94758202d2
Arun Rao Balappa <arunraobalappa@gmail.com>
2023-10-27 16:13:06 +0530

    complete code overhaul, all three files.

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-/* script.js */
-function setReminder() {
-    const task = document.getElementById("task").value;
-    const time = document.getElementById("time").value;
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("task");
+    const timeInput = document.getElementById("time");
+    const setReminderButton = document.getElementById("set-reminder");
+    const reminderList = document.getElementById("reminder-list");
 
-    if (task && time) {
-        const reminderTime = new Date(time).getTime();
-        const currentTime = new Date().getTime();
-        const timeUntilReminder = reminderTime - currentTime;
+    setReminderButton.addEventListener("click", function () {
+        const task = taskInput.value;
+        const time = timeInput.value;
 
-        if (timeUntilReminder > 0) {
-            setTimeout(() => {
-                alert(`Reminder: ${task}`);
-            }, timeUntilReminder);
+        if (task && time) {
+            const reminderTime = new Date(time).toLocaleString();
+
+            const reminderItem = document.createElement("li");
+            reminderItem.textContent = `Task: ${task}, Time: ${reminderTime}`;
+            reminderList.appendChild(reminderItem);
+
+            taskInput.value = "";
+            timeInput.value = "";
         } else {
-            alert("Please choose a future time for your reminder.");
+            alert("Please fill in both task and time fields.");
         }
-    } else {
-        alert("Please fill in both task and time fields.");
-    }
-}
+    });
+});
